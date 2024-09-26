249326c4b30ff2441fd0e9d5654b1daf59404708
Bruno Carvalho da Costa <brunoccst@gmail.com>
2024-09-26 22:08:28 +0200

    Importing Roboto font

--- portfolio.web/src/main.tsx
+++ portfolio.web/src/main.tsx
@@ -4,6 +4,10 @@ import { App } from 'components/app'
 import { LoadingSpinner } from 'components/loading-spinner';
 import "./i18n/config";
 import './main.scss'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
