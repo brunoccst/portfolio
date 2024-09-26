458f8a46307794ad539e700ec746c1814cd2c0f7
Bruno Carvalho da Costa <brunoccst@gmail.com>
2024-09-26 15:04:03 +0200

    Included LanguageDetector

--- portfolio.web/src/i18n/config.ts
+++ portfolio.web/src/i18n/config.ts
@@ -1,13 +1,14 @@
 import i18n from "i18next";
 import HttpApi from "i18next-http-backend";
+import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
 import { SupportedLngs } from "./supported-lngs";
 
 i18n
     .use(HttpApi)
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        lng: "en-US",
         fallbackLng: {
             "en": ["en-US"],
             "default": ["en-US"]
