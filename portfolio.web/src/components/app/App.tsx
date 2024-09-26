336e74601a78044584ec3f3de7c2ffb68a709bf1
Bruno Carvalho da Costa <brunoccst@gmail.com>
2024-09-26 13:58:33 +0200

    Added base configuration and translation files

--- portfolio.web/src/components/app/App.tsx
+++ portfolio.web/src/components/app/App.tsx
@@ -1,10 +1,13 @@
 import './App.scss'
 import { AboutMe } from 'components/about-me'
+import { useTranslation } from "react-i18next";
 
 export const App = () => {
+  const { t } = useTranslation();
+
   return (
     <>
-      <div>My Portfolio</div>
+      <h2>{t("Welcome")}</h2>
       <AboutMe />
     </>
   )
--- portfolio.web/src/main.tsx
+++ portfolio.web/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from 'components/app'
+import "./i18n/config";
 import './main.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
