6680292cf6b5846a2db516426ea4d2c7302ca59b
Bruno Carvalho da Costa <brunoccst@gmail.com>
2024-09-29 17:01:38 +0200

    Changed loads

--- portfolio.web/src/main.tsx
+++ portfolio.web/src/main.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from 'components/App'
 import CircularProgress from '@mui/material/CircularProgress';
+import { MUIThemeWrapper } from 'mui/MUIThemeWrapper';
+import { App } from 'components/App'
 import "./i18n/config";
 import './main.scss'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { MUIThemeWrapper } from 'mui/MUIThemeWrapper';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
