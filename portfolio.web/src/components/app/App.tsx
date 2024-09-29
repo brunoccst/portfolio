9e8ebaac87cb694faa8ac3a2ba0c781217e7f672
Bruno Carvalho da Costa <brunoccst@gmail.com>
2024-09-29 17:06:03 +0200

    Changed import order in App.tsx

--- portfolio.web/src/components/app/App.tsx
+++ portfolio.web/src/components/app/App.tsx
@@ -1,12 +1,12 @@
 import './App.scss';
 import { Navbar } from 'components/layout/Navbar';
 import { Sidebar } from 'components/layout/Sidebar';
+import { Footer } from 'components/layout/Footer';
 import { WelcomeBanner } from 'components/sections/WelcomeBanner';
 import { AboutMe } from 'components/sections/AboutMe'
 import { Skills } from 'components/sections/Skills';
 import { Experiences } from 'components/sections/Experiences';
 import useLocalizeDocumentAttributes from "i18n/useLocalizeDocumentAttributes";
-import { Footer } from 'components/layout/Footer';
 
 export const App = () => {
   useLocalizeDocumentAttributes();
