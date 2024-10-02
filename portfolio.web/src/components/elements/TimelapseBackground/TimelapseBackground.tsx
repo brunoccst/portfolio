db2390b332a8571d79003d11dec95d171c6f23c0
Bruno Carvalho da Costa <brunoccst@gmail.com>
2024-10-02 18:02:06 +0200

    Fixed inital transition on dark mode

--- portfolio.web/src/components/elements/TimelapseBackground/TimelapseBackground.tsx
+++ portfolio.web/src/components/elements/TimelapseBackground/TimelapseBackground.tsx
@@ -1,13 +1,27 @@
 import "./TimelapseBackground.scss";
+import { useEffect, useState } from "react";
 import { useColorScheme } from "@mui/material";
 import Mountain from "assets/mountain.png";
 
 export const TimelapseBackground = () => {
     const { mode, systemMode } = useColorScheme();
+    const [transitionEnabled, setTransitionEnabled] = useState(false);
     const isDarkMode = systemMode === "dark" || mode === "dark";
 
+    // Enable transitions after the component mounts
+    useEffect(() => {
+        const timeout = setTimeout(() => {
+            setTransitionEnabled(true);
+        }, 100);
+
+        return () => clearTimeout(timeout);
+    }, []);
+
     return (
-        <div id="timelapse-background" className={isDarkMode ? "dark" : ""}>
+        <div
+            id="timelapse-background"
+            className={`${isDarkMode ? "dark" : ""} ${transitionEnabled ? "transition-enabled" : ""}`}
+        >
             <img src={Mountain} />
         </div>
     );
